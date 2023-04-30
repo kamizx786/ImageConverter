95ebac6a05c8e2511e4e37c75e49166c9dfd6130
Kamran Ali <Kamran Ali@KAMRAN-ALI>
2023-05-01 01:54:20 +0500

    HEIC

--- routes/convert.js
+++ routes/convert.js
@@ -3,12 +3,8 @@ const router=express.Router();
 import {ConvertAVIFtoSVG, ConvertWEBPtoSVG, ConverttoAVIF, ConverttoGIF, ConverttoICO, ConverttoJPG, ConverttoPNG, ConverttoSVG, ConverttoTIFF, ConverttoWEBP} from "../controllers/convert";
 import formidable from "express-formidable";
 
-router.post("/convert-to-jpg",formidable({maxFileSize: 5 * 1024 * 1024,
-    multiples: true,
-    maxFieldsSize: 50 * 1024 * 1024}),ConverttoJPG);
-router.post("/convert-to-png",formidable({maxFileSize: 5 * 1024 * 1024,
-    multiples: true,
-    maxFieldsSize: 50 * 1024 * 1024}),ConverttoPNG);
+router.post("/convert-to-jpg",formidable({maxFileSize: 5 * 1024 * 1024}),ConverttoJPG);
+router.post("/convert-to-png",formidable({maxFileSize: 5 * 1024 * 1024}),ConverttoPNG);
 router.post("/convert-to-gif",formidable({maxFileSize:5*1024*1024}),ConverttoGIF);
 router.post("/convert-to-avif",formidable({maxFileSize:5*1024*1024}),ConverttoAVIF);
 router.post("/convert-to-tiff",formidable({maxFileSize:5*1024*1024}),ConverttoTIFF);
